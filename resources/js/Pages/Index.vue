90a79bc000062b94b5e1bc7e4e0f78625f4c49c9
Rizky Dhani Ismail <rizkydhani15@gmail.com>
2023-08-23 15:29:46 +0800

    add example Index & Show Page Vue

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <h1>Hello</h1>
+        <Link href="/show">Show Page</Link>
+    </div>
+</template>
+<script setup>
+    import {Link} from '@inertiajs/vue3';
+</script>
---
+++ resources/js/Pages/Show.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <h1>Show</h1>
+        <Link href="/">Index Page</Link>
+    </div>
+</template>
+
+<script setup>
+    import {Link} from '@inertiajs/vue3';
+</script>
